764f6328b2b3e4b764ffba44fea8de84ae04d356
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2022-09-14 18:05:43 +0530

    Initial framework - In Progress
    Design for "Today in History"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ export class AppComponent {
   title = 'mdb-angular-ui-kit-free';
   dummyLaunchImg: string = "https://nyc3.digitaloceanspaces.com/spacelaunchnow-prod-east/media/launcher_images/long2520march25204_image_20190520170012.jpeg";
   picofday: string = "https://apod.nasa.gov/apod/image/2209/SnakingFilament_Friedman_960.jpg";
-
+  astronaut="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/astronaut_images/albert2520sacco_image_20190211162452.jpeg";
 }
